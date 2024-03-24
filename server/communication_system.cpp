17c91b53ca95d88964facbe718f9974398349cef
thorc <thorvath6447@conestogac.on.ca>
2024-03-24 18:20:12 -0400

    Commit No.2

--- server/communication_system.cpp
+++ server/communication_system.cpp
@@ -4,28 +4,36 @@ namespace GroundControl {
 
 #pragma comment(lib, "ws2_32.lib")
 
-    GroundControl::GroundControl(int port) : port_(port), listenSocket_(INVALID_SOCKET) {}
+    GroundControl::GroundControl()
+    {
+    }
 
-    GroundControl::~GroundControl() {
-        if (listenSocket_ != INVALID_SOCKET) {
+    GroundControl::~GroundControl() 
+    {
+        if (listenSocket_ != INVALID_SOCKET) 
+        {
             closesocket(listenSocket_);
         }
         WSACleanup();
     }
 
-    bool GroundControl::Initialize() {
+    bool GroundControl::Initialize() 
+    {
         WSADATA wsaData;
         int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
-        if (result != 0) {
+        if (result != 0) 
+        {
             std::cerr << "WSA Startup has an issue. Result is non-zero." << std::endl;
             return false;
         }
         return true;
     }
 
-    bool GroundControl::Connect() {
+    bool GroundControl::Connect(int port) // connecting based on a port allows for us to specify the simualed frequency for the commuinication channel
+    { 
         listenSocket_ = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-        if (listenSocket_ == INVALID_SOCKET) {
+        if (listenSocket_ == INVALID_SOCKET) 
+        {
             std::cerr << "Error opening the socket." << std::endl;
             return false;
         }
@@ -33,15 +41,17 @@ namespace GroundControl {
         sockaddr_in serverAddr;
         serverAddr.sin_family = AF_INET;
         serverAddr.sin_addr.s_addr = INADDR_ANY;
-        serverAddr.sin_port = htons(static_cast<u_short>(port_));
+        serverAddr.sin_port = htons(static_cast<u_short>(port));
 
-        if (bind(listenSocket_, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr)) == SOCKET_ERROR) {
+        if (bind(listenSocket_, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr)) == SOCKET_ERROR) 
+        {
             std::cerr << "Bind failed with error: " << WSAGetLastError() << std::endl;
             closesocket(listenSocket_);
             return false;
         }
 
-        if (listen(listenSocket_, SOMAXCONN) == SOCKET_ERROR) {
+        if (listen(listenSocket_, SOMAXCONN) == SOCKET_ERROR) 
+        {
             std::cerr << "Listen failed with error: " << WSAGetLastError() << std::endl;
             closesocket(listenSocket_);
             return false;
@@ -51,20 +61,27 @@ namespace GroundControl {
     }
 
 
-    bool GroundControl::SendMessage(const std::string& message) { // no idea what this looks like atm
-        if (message == SWITCH_CHANNEL_COMMAND) {
+    bool GroundControl::SendMessage(const std::string& message) 
+    { // no idea what this looks like atm
+        if (message == SWITCH_CHANNEL_COMMAND) 
+        {
             std::string channelSwitchMessage = "Please switch to channel X"; 
             // Send the channel switch message to the client
             return true; 
-        } else {
+        } else 
+        {
             // Message is not a channel switch command, send it normally
             return false; 
         }
     }
 
-    std::string GroundControl::ReceiveMessage() {
+
+
+    std::string GroundControl::ReceiveMessage() 
+    {
         SOCKET clientSocket = accept(listenSocket_, NULL, NULL);
-        if (clientSocket == INVALID_SOCKET) {
+        if (clientSocket == INVALID_SOCKET) 
+        {
             std::cerr << "Accept failed with error: " << WSAGetLastError() << std::endl;
             closesocket(listenSocket_);
             WSACleanup();
@@ -73,15 +90,27 @@ namespace GroundControl {
 
         char buffer[1024];
         int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
-        if (bytesReceived == SOCKET_ERROR) {
+        if (bytesReceived == SOCKET_ERROR) 
+        {
             std::cerr << "Receive failed with error: " << WSAGetLastError() << std::endl;
             closesocket(clientSocket);
             return "";
         }
         buffer[bytesReceived] = '\0';
         std::string receivedMessage(buffer);
+
+        // do something with the message, ig log it
+
         closesocket(clientSocket);
         return receivedMessage;
+    }
+
+    void GroundControl::HandleATCToATCHandoffRequest(GroundControl* requestingServer, GroundControl* targetServer)
+    {
+    }
+
+    void GroundControl::HandleATCToAircraftHandoffRequest(GroundControl* requestingServer, char* targetAircraft)
+    {
     }
 
 }
--- server/communication_system.h
+++ server/communication_system.h
@@ -5,6 +5,7 @@
 #include <ws2tcpip.h> 
 #include <iostream>
 #include <string>
+#include <vector>
 
 namespace GroundControl {
 
@@ -12,19 +13,26 @@ namespace GroundControl {
     const unsigned int DEFAULT_CHANNEL = 5555;
 
     enum class CommunicationType { VHF, HF };
+    // maybe some sort of channel for ground control to communicate over
 
     class GroundControl 
     {
     public:
-        GroundControl(int port); // constructor wit da parameters
+        GroundControl(); // constructor wit da parameters
         ~GroundControl(); // deconstructor
         bool Initialize();
-        bool Connect();
-        bool SendMessage(const std::string& message);
+        bool Connect(int port);
+        bool SendMessage(const std::string& message); // might not actually be used
         std::string ReceiveMessage();
+
+        // maybe should be broken down into a function that requests a hand off and one that accepts it
+        void HandleATCToATCHandoffRequest(GroundControl* requestingServer, GroundControl* targetServer); 
+        void HandleATCToAircraftHandoffRequest(GroundControl* requestingServer, char* targetAircraft);
     private:
         int port_;
         SOCKET listenSocket_;
+        std::vector<GroundControl*> connectedATCServers;
+        std::vector<char*> connectedAircrafts; // this will be an ip that represents each of the aircrafts in flight
     };
 
 }
